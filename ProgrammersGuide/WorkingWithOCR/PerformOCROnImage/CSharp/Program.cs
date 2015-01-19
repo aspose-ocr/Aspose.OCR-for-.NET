4de051f0ed5cbdc13c84ed7cb8e57b931f8701aa
FatimaAhmed <fatima.rabbiya@aspose.com>
2015-01-19 10:47:07 +0500

    Example updated.

--- ProgrammersGuide/WorkingWithOCR/PerformOCROnImage/CSharp/Program.cs
+++ ProgrammersGuide/WorkingWithOCR/PerformOCROnImage/CSharp/Program.cs
@@ -19,36 +19,17 @@ namespace PerformOCROnImage
             // The path to the documents directory.
             string dataDir = Path.GetFullPath("../../../Data/");
 
-            // Resource file
-            string resourceFilePath = dataDir+ "Resource.zip";
-            // Source file: the file on which OCR will be performed.
-            string imageFile = dataDir + "Sampleocr.bmp";
+            //Initialize an instance of OcrEngine
+            OcrEngine ocrEngine = new OcrEngine();
 
-            Console.WriteLine("Performing OCR on " + imageFile + "....");
+            //Set the Image property by loading the image from file path location or an instance of MemoryStream 
+            ocrEngine.Image = ImageStream.FromFile(dataDir + "Sampleocr.bmp");
 
-            // Initialize OcrEngine.
-            OcrEngine ocr = new OcrEngine();
-            // Set the image.
-            ocr.Image = ImageStream.FromFile( imageFile);
-            // Add language.
-            ocr.Languages.AddLanguage(Language.Load("english"));
-            // Load the resource file.
-            using (ocr.Resource = new FileStream(resourceFilePath, FileMode.Open))
+            //Process the image
+            if (ocrEngine.Process())
             {
-                try
-                {
-                    // Process the whole image
-                    if (ocr.Process())
-                    {
-                        // Get the complete recognized text found from the image
-                        Console.WriteLine("Text recognized: " + ocr.Text);
-                        File.WriteAllText(dataDir + "Output.txt", ocr.Text.ToString());
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Exception: " + ex.ToString());
-                }
+                //Display the recognized text
+                Console.WriteLine(ocrEngine.Text);
             }
         }
     }
