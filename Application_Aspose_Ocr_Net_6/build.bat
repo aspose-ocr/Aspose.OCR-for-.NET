e3f7a45c436401f6cf3b610cea70618f75b09b7c
Anna Samoluk <annsamoluk82@gmail.com>
2024-01-26 17:10:09 +0200

    fix build bat file

--- Application_Aspose_Ocr_Net_6/build.bat
+++ Application_Aspose_Ocr_Net_6/build.bat
@@ -1,3 +1,4 @@
 ::set PATH=%WINDIR%\Microsoft.Net\Framework64\v3.5;%PATH%
 dotnet restore
-dotnet msbuild ApplicationsAsposeOcr.sln  -t:Rebuild -p:Configuration=Release -v:n
+dotnet msbuild Recognize.sln  -t:Rebuild -p:Configuration=Release -v:n
+pause()
